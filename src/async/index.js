5c921fdcc1383948203ce76b58a784483a20181e
Eleazar <eleazarcaz@outlook.com>
2020-03-01 20:09:14 -0600

    Conociendo async-await

---
+++ src/async/index.js
@@ -0,0 +1,29 @@
+const doSomethingAsync = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Do Something Async'), 3000)
+      : reject(new Error('Error'));
+  });
+};
+
+const doSomething = async () => {
+  const something = await doSomethingAsync();
+  console.log(something);
+};
+
+console.log('Before');
+doSomething();
+console.log('After');
+
+const doSomethingOther = async () => {
+  try {
+    const something = await doSomethingAsync();
+    console.log(something);
+  } catch {
+    console.error(error);
+  }
+};
+
+console.log('Before');
+doSomethingOther();
+console.log('After');
